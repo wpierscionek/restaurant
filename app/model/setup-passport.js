bed23e55d512fce821f6129970482ba46c2f96e2
wpierscionek <pierscionekW@gmail.com>
2016-07-26 22:43:54 -0400

    7/26

--- app/model/setup-passport.js
+++ app/model/setup-passport.js
@@ -2,10 +2,10 @@ var passport = require('passport');
 var Auth0Strategy = require('passport-auth0');
 
 var strategy = new Auth0Strategy({
-    domain:       'rbla3066.auth0.com',
-    clientID:     'dCLsz1hEmVYS3KFLS9pUT2LeVFitNRhO',
+    domain: 'rbla3066.auth0.com',
+    clientID: 'dCLsz1hEmVYS3KFLS9pUT2LeVFitNRhO',
     clientSecret: '7aYE3BvRnRHqjq9SYhA6BiVaHAnV5rr4FPVcKxpVajMpky5IBq7X0PmOHsQo8BBY',
-    callbackURL:  'https://experiod.herokuapp.com/user'
+    callbackURL: 'https://experiod00.herokuapp.com/user'
 }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
